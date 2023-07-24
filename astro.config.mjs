679ae832769186941e9b968b0e071bbaa706aa09
nseepana <seepana.naresh@gmail.com>
2023-07-24 16:49:47 +0530

    update meta and seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,22 @@
 import { defineConfig } from 'astro/config'
 import starlight from '@astrojs/starlight'
-import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap'
 
-
+// const siteAssets = "/site-asstes"
 // https://astro.build/config
 export default defineConfig({
   // output: 'server',
   // adapter: vercel({
   //   analytics: true,
   // }),
+  site: "https://dp.tamphus.com/",
   integrations: [
+// ref: https://docs.astro.build/en/guides/integrations-guide/sitemap/
+    sitemap({
+      changefreq: 'weekly',
+      priority: 0.7,
+      lastmod: new Date(),
+    }),
     starlight({
       title: 'Design Patterns',
       social: {
@@ -24,6 +31,106 @@ export default defineConfig({
             'defer': true,
           },
         },
+        {
+          tag: 'meta',
+          attrs: { property: 'og:title', content: 'Design Patterns' },
+        },        
+        {
+          tag: 'meta',
+          attrs: { property: 'og:description', content: 'Design Patterns' },
+        },        
+        {
+          tag: 'meta',
+          attrs: { property: 'robots', content: 'index, follow' },
+        },        
+        {
+          tag: 'meta',
+          attrs: { property: 'keywords', content: 'Design Patterns,Design Patterns real world scenarios, dp tamphus, design patterns tamphus, chatGPT design patterns, naresh design patterns,Software Design Patterns,Object-Oriented Design Patterns,Creational Patterns,Structural Patterns,Behavioral Patterns,Gang of Four Patterns,Singleton Pattern,Factory Pattern,Observer Pattern,Strategy Pattern,Adapter Pattern,Decorator Pattern,MVC Pattern (Model-View-Controller),Abstract Factory Pattern,Template Method Pattern,Command Pattern,Prototype Pattern,Mediator Pattern,Composite Pattern,Modern Design Patterns,Design Patterns in Java, C++, Python,Design Patterns for Web Development,Design Patterns for Beginners,Design Patterns for Experienced Developers,Design Patterns Cheat Sheet,Design Patterns Examples and Implementations,Best Practices for Design Patterns' },
+        },
+       
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '57x57', href: '/assets/apple-icon-57x57.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '60x60', href: '/assets/apple-icon-60x60.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '72x72', href: '/assets/apple-icon-72x72.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '76x76', href: '/assets/apple-icon-76x76.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '114x114', href: '/assets/apple-icon-114x114.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '120x120', href: '/assets/apple-icon-120x120.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '144x144', href: '/assets/apple-icon-144x144.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '152x152', href: '/assets/apple-icon-152x152.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/apple-icon-180x180.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/assets/android-icon-192x192.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/assets/favicon-32x32.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'icon', type: 'image/png', sizes: '96x96', href: '/assets/favicon-96x96.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/assets/favicon-16x16.png' }
+          },
+          {
+            tag: 'link',
+            attrs: { rel: 'manifest', href: '/assets/manifest.json' }
+          },
+          {
+            tag: 'meta',
+            attrs: { name: 'msapplication-TileColor', content: '#ffffff' }
+          },
+          {
+            tag: 'meta',
+            attrs: { name: 'msapplication-TileImage', content: '/assets/ms-icon-144x144.png' }
+          },
+          {
+            tag: 'meta',
+            attrs: { name: 'theme-color', content: '#ffffff' }
+          },
+                       
+        // {
+        //   tag: 'meta',
+        //   attrs: { property: 'og:image', content: 'Design Patterns' },
+        // },        
+        {
+          tag: 'meta',
+          attrs: { property: 'og:locale', content: 'en_US' },
+        }, 
+        {
+          tag: 'meta',
+          attrs: { property: 'language', content: 'english' },
+        }, 
+
+               
       ],
       sidebar: [
         {
